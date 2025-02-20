0fecfae2b1798a1ae695d5d52711cb08794fbfde
unknown <pedrovalente_00@hotmail.com>
2025-02-20 16:14:18 +0100

    using vercel.json again

--- api/index.js
+++ api/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get("*", (req,res) => {
-    res.send("Page not found D;")
-})
+app.get("*", (req,res) => {res.send("Page not found D;")})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server ready on port", PORT));
 
