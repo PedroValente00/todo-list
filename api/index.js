d7709c085c2041f417044d7d59cb0e3cac8ab3d9
unknown <pedrovalente_00@hotmail.com>
2025-02-20 18:54:17 +0100

    ...

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist'), {index:false}));  
+// app.use(express.static(path.join(__dirname, '/dist')));  
 
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
 app.get("*", (req,res) => {res.send("Page not found D;")})
