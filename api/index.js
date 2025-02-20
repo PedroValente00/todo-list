5dae41ba6fa913ddbc31357412a04768ad506cdb
unknown <pedrovalente_00@hotmail.com>
2025-02-20 19:47:11 +0100

    .

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
 app.get("/*", (req,res) => {res.send("Page not found D;")})
 app.get("/b", (req,res) => {res.send("Don't go to b")})
 app.get("/here", (req,res) => {res.send("You are here")})
+app.get("there", (req,res) => {res.send("You are there")})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server ready on port", PORT));
 
