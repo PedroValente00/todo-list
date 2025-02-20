4af80408ddd47d49b13a620eecc5e3c401b993ff
unknown <pedrovalente_00@hotmail.com>
2025-02-20 16:07:27 +0100

    changed middleware order

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get("*", (req,res) => {
     res.send("Page not found D;")
 })
