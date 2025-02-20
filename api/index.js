c640fb38ecd957bbbb0e191190b3fd03a1fc67d4
unknown <pedrovalente_00@hotmail.com>
2025-02-20 14:54:42 +0100

    middlewares

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("/dist"))
 app.use(express.static("/"))
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
 const PORT = process.env.PORT || 3000;
