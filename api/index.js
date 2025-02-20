bda9535e3c8eedde8fdccb691fe8964a711d15e9
unknown <pedrovalente_00@hotmail.com>
2025-02-20 15:06:58 +0100

    using path.join for middleware

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("api"))
-app.use(express.static("/api"))
-app.use(express.static("api/dist"))
-app.use(express.static("/api/dist"))
-app.use(express.static("/dist"))
-app.use(express.static("dist"))
-app.use(express.static("/"))
+// app.use(express.static("api"))
+// app.use(express.static("/api"))
+// app.use(express.static("api/dist"))
+// app.use(express.static("/api/dist"))
+// app.use(express.static("/dist"))
+// app.use(express.static("dist"))
+// app.use(express.static("/"))
+
+const path = require("path")
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
 const PORT = process.env.PORT || 3000;
 
