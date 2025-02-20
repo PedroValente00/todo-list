ae5993de2be61b36d372e767e43824271af8f38b
unknown <pedrovalente_00@hotmail.com>
2025-02-20 19:43:14 +0100

    ffs

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname, '/dist')));
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
 app.get("/*", (req,res) => {res.send("Page not found D;")})
 app.get("/b", (req,res) => {res.send("Don't go to b")})
+app.get("/here", (req,res) => {res.send("You are here")})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server ready on port", PORT));
 
