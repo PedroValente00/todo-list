f4af92cf5941d808e7d14f2019f19ff8f1f493e3
unknown <pedrovalente_00@hotmail.com>
2025-02-20 18:35:30 +0100

    .

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')));
+// app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
 app.get("*", (req,res) => {res.send("Page not found D;")})
