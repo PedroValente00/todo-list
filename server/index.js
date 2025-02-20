fc69f8819084d49eeb42adf6310674acf1c1d6ac
unknown <pedrovalente_00@hotmail.com>
2025-02-20 14:25:52 +0100

    push

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("dist"))
-
-const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
+app.use(express.static("dist"))
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log("Server ready on port", PORT));
 
 module.exports = app;
