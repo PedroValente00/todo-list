1c03e592a8850354f2132b23bb881d515b81a04e
unknown <pedrovalente_00@hotmail.com>
2025-02-21 15:37:39 +0100

    .

--- api/index.js
+++ api/index.js
@@ -6,10 +6,10 @@ app.use(express.static(path.join(__dirname, '/dist')));
 
 
 app.get("/", (req, res) => res.send(`Hello from Express on port ${PORT} `));
-app.get("/b", (req,res) => {res.sendFile("b.html")})
+app.get("/b", (req,res) => {res.sendFile("/b.html")})
 app.get("/here", (req,res) => {res.send("You are here")})
 
-// app.get("/*", (req,res) => {res.send("Page not found...")})
+app.get("/*", (req,res) => {res.send("Page not found...")})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server ready on port", PORT));
