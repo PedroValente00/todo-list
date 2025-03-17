1ed2aef018a3c5058257fdbb40fa830dc5b19a1c
unknown <pedrovalente_00@hotmail.com>
2025-03-17 13:30:44 +0100

    .

--- vite/src/components/SubmitForm/SubmitNew.jsx
+++ vite/src/components/SubmitForm/SubmitNew.jsx
@@ -17,9 +17,9 @@ export default function SubmitNew({toDos, setToDos}) {
         const capitalizedNewToDo = `${capFirst}${capRest}`
         const submission = {id:uuid(), toDo:capitalizedNewToDo, done:false }
         //do database later with authentication
+        axios.post('/api/toDos', submission);
         setToDos(toDos => [...toDos, submission])
         setNewToDo("")
-        axios.post('/api/toDos', submission);
     }
 
     return <form action="/api/toDos" method="post" id="newTodoForm"
