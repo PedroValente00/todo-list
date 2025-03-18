46c6e91366c5d531d1073a896fec59a0c76e7c46
unknown <pedrovalente_00@hotmail.com>
2025-03-18 15:17:23 +0100

    trying to connect mongo... I hate Vercel

--- api/index.js
+++ api/index.js
@@ -2,23 +2,24 @@ const express = require("express");
 const path = require("path")
 const app = express();
 const { v4: uuid } = require('uuid');
-// const ToDo = require("./database")
+const ToDo = require("./database")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '/dist')));
 
-// app.get("/api/toDos",async (req, res) => {
-//     const data = await ToDo.find({})
-//     res.send(data)
-// })
-// app.post("/api/toDos", async (req, res) => {
-//     const toDo = new ToDo(req.body)
-//     await toDo.save();
-//     res.send("to do submission received")
-// })
+app.get("/api/toDos",async (req, res) => {
+    const data = await ToDo.find({})
+    console.log(data)
+    res.send(data)
+})
+app.post("/api/toDos", async (req, res) => {
+    const toDo = new ToDo(req.body)
+    await toDo.save();
+    res.send("to do submission received")
+})
 
-app.get("/save", async (req, res) => {
+app.get("/api/save", async (req, res) => {
     res.send("Request received")
 })
 
