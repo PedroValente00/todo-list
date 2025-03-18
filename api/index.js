7cd527001e86878f8918cc1dfd018e948ffb53be
unknown <pedrovalente_00@hotmail.com>
2025-03-18 14:17:08 +0100

    removing database stuff

--- api/index.js
+++ api/index.js
@@ -2,32 +2,23 @@ const express = require("express");
 const path = require("path")
 const app = express();
 const { v4: uuid } = require('uuid');
-const ToDo = require("./database")
+// const ToDo = require("./database")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
-// const data = [
-//     { id: uuid(), toDo: "Do the dishes", done: true },
-//     { id: uuid(), toDo: "Take out the trash", done: false }
-// ]
-
 app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get("/api/toDos",async (req, res) => {
-    const data = await ToDo.find({})
-    res.send(data)
-})
-app.post("/api/toDos", async (req, res) => {
-    // data.push(req.body)
-    const toDo = new ToDo(req.body)
-    await toDo.save();
-
-    res.send("to do submission received")
-})
+// app.get("/api/toDos",async (req, res) => {
+//     const data = await ToDo.find({})
+//     res.send(data)
+// })
+// app.post("/api/toDos", async (req, res) => {
+//     const toDo = new ToDo(req.body)
+//     await toDo.save();
+//     res.send("to do submission received")
+// })
 
 app.get("/save", async (req, res) => {
-    
     res.send("Request received")
 })
 
